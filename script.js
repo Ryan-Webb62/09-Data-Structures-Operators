cf3ffcc6b8b9936f6307a013331ed862456f5907
Ryan <65420906+Ryan-Webb62@users.noreply.github.com>
2021-02-14 11:40:25 -0500

    Lecture on Optional Chaining

--- script.js
+++ script.js
@@ -46,7 +46,31 @@ const restaurant = {
     console.log(mainIngredent, otherIngredents);
   },
 };
-//---------
+//---------Optional Chaining (?.)----
+
+if (restaurant.openingHours && restaurant.openingHours.mon)
+  console.log(restaurant.openingHours.mon.open);
+// WITHOUT optional chaining - get error
+// console.log(restaurant.openingHours.mon.open);
+
+// WITH optional chaining - ? comes after the element we are testing
+console.log(restaurant.openingHours.mon?.open);
+console.log(restaurant.openingHours?.mon?.open);
+
+//  Example
+const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+for (const day of days) {
+  const open = restaurant.openingHours[day]?.open ?? 'closed';
+  console.log(`On ${day}, we open at ${open}`);
+}
+
+// Methods
+console.log(restaurant.order?.(0, 1) ?? 'Method does not exist');
+console.log(restaurant.orderRisotto?.(0, 1) ?? 'Method does not exist');
+
+// Arrays
+const users = [{ name: 'Jonas', email: 'hello@jonas.io' }];
+console.log(users[0]?.name ?? 'User array empty');
 
 //---------The for-of Loop----------
 /* 
