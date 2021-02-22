fc7e6610f098b57d12697262639921a981fafdd0
Ryan <65420906+Ryan-Webb62@users.noreply.github.com>
2021-02-22 08:49:15 -0500

    Lecture on MAPS: Basics

--- script.js
+++ script.js
@@ -46,7 +46,45 @@ const restaurant = {
     console.log(mainIngredent, otherIngredents);
   },
 };
+
+//-------------Maps:Fundamentals-----------------
+
+const rest = new Map();
+rest.set('name', 'Classico Italiano');
+rest.set(1, 'Firenze, Italy');
+console.log(rest.set(2, 'Lisbon, Portugal'));
+
+rest
+  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'We are open :D')
+  .set(false, 'We are closed :(');
+
+console.log(rest.get('name'));
+console.log(rest.get(1));
+console.log(rest.get(true));
+
+const time = 21;
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+console.log(rest.has('categories'));
+rest.delete(2);
+console.log(rest);
+console.log(rest.size);
+// rest.clear();
+// console.log(rest);
+
+const arr = [1, 2];
+rest.set(arr, 'Test');
+rest.set(document.querySelector('h1'), 'Heading');
+console.log(rest);
+console.log(rest.size);
+
+console.log(rest.get(arr));
+
 //-------------Sets------------------------------
+/* 
 const ordersSet = new Set([
   'Pasta',
   'Pizza',
@@ -80,6 +118,7 @@ console.log(
   new Set(['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter']).size
 );
 console.log(new Set('jonasschmedtmann').size);
+ */
 //-----------Challenge 2 ------------------------
 /* 
 const game = {
