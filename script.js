d870042ea5456d0f4f6fa097a69a63bb8f758dd4
Ryan <65420906+Ryan-Webb62@users.noreply.github.com>
2021-03-07 11:40:39 -0500

    String Methods Practice

--- script.js
+++ script.js
@@ -46,7 +46,26 @@ const restaurant = {
     console.log(mainIngredent, otherIngredents);
   },
 };
+//---------------String Methods Practice ----------------
+// Data needed for a later exercise
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+const getCode = str => str.slice(0, 3).toUpperCase();
+
+for (const flight of flights.split('+')) {
+  const [type, from, to, time] = flight.split(';');
+  const output = `${type.startsWith('_Delayed') ? '🔴' : ''}${type.replaceAll(
+    '_',
+    ' '
+  )} ${getCode(from)} => ${getCode(to)} (${time.replace(':', 'h')})`.padStart(
+    50
+  );
+  console.log(output);
+}
+
 //---------------Challenge #4 ---------------------------
+/* 
 document.body.append(document.createElement('textarea'));
 document.body.append(document.createElement('button'));
 
@@ -68,6 +87,7 @@ btn.addEventListener('click', function () {
     console.log(cammelCase, '✅'.repeat(index + 1));
   }
 });
+ */
 //--------------Working with Strings Part III------------
 /* 
 console.log('a+very+nice+string'.split('+'));
